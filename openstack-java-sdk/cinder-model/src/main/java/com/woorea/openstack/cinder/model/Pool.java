7e5358325be6a15fff61aeef98cf19009f90026c
Varuneshwar Gudisena <vg411h@att.com>
2017-04-20 15:45:02 -0500

    Deleted pool.java at invalid location

--- openstack-java-sdk/cinder-model/src/main/java/com/woorea/openstack/cinder/model/Pool.java
+++ openstack-java-sdk/cinder-model/src/main/java/com/woorea/openstack/cinder/model/Pool.java
@@ -1,3 +1,6 @@
+/*******************************************************************************
+ * Copyright (c) 2017 AT&T Intellectual Property. All rights reserved.
+ *******************************************************************************/
 package com.woorea.openstack.cinder.model;
 
 import java.io.Serializable;
